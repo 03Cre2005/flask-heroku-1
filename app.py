58dc40dfac382b5ab150a33ff966e395d95bf677
03Cre2005 <87839987+03Cre2005@users.noreply.github.com>
2021-08-20 10:17:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-    return"<font color=red>มลทิพย์ ตั้งภานิช</font> <br>เลขที่ 17 ชั้นม.4/10"
+    return "<font color=red>มลทิพย์ ตั้งภานิช</font> <br>เลขที่ 17 ชั้นม.4/10"
 
 @app.route('/hello/<string:name>')
 def Home(name):
