f7f702e463cd0c565f1bdfd213bb2ca2b6f996d8
03Cre2005 <87839987+03Cre2005@users.noreply.github.com>
2021-08-20 09:18:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-    return"มลทิพย์ ตั้งภานิช เลขที่ 17 ชั้นม.4/10"    
+    return"มลทิพย์ ตั้งภานิช <br>เลขที่ 17 ชั้นม.4/10"    
 
 if __name__ == "__main__":
     app.run(debug=False)
